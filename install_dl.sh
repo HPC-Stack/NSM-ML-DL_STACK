ef7e625bf9579e513bbaf37747eea6ce628a37fb
Saurabh Deshkar <116559570+SaurabhYDeshkar@users.noreply.github.com>
2026-09-17 09:48:38 +0530

    locking miniconda version and handling some path definitions

--- install_dl.sh
+++ install_dl.sh
@@ -3,11 +3,12 @@
 
 # Define and create installation directory if it doesn't exist
 export INSTALLATION_PATH=/home/apps/MLDL/DL-CondaPy3.10
+export MODULE_PATH=/home/apps/modulefiles/MLDL
 mkdir -p $INSTALLATION_PATH
 
 # Download and install Miniconda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -u -p $INSTALLATION_PATH
+wget https://repo.anaconda.com/miniconda/Miniconda3-py313_26.7.1-1-Linux-x86_64.sh
+bash Miniconda3-py313_26.7.1-1-Linux-x86_64.sh -b -u -p $INSTALLATION_PATH
 
 # Initialize conda
 source $INSTALLATION_PATH/bin/activate
@@ -18,4 +19,5 @@ for yml_file in `ls YMLfiles/*.yml`; do
     conda env create -f $yml_file
 done
 
-cp modulefiles/* /home/apps/modulefiles/MLDL/
+mkdir -p $MODULE_PATH
+cp modulefiles/* $MODULE_PATH
